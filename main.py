97a698c979f4a3aca9dcf789491e228c8ec34e5b
Hans Magnaye <55745065+jellomarr@users.noreply.github.com>
2022-06-10 16:11:20 +1000

    Update main.py

--- main.py
+++ main.py
@@ -15,9 +15,6 @@ session.auth = AWS4Auth(
     session_token=credentials.token
 )
 
-#get the news data
-SearchTerm = "Azure"
-
 #the RSS link feed 
 RSSLink = 'https://www.itnews.com.au/RSS/rss.ashx?type=Category&ID=977'
 
@@ -35,24 +32,11 @@ for item in items:
   description = item.description.text
   link = item.link.text
   pubDate = item.pubDate.text
-  if SearchTerm in title:
-    print(f"Title: {title}\n\nDescription: {description}\n\nLink: {link}\n\npubDate: {pubDate}\n\n----------\n\n")
-    #make this a module
-    query = f"""
-        mutation MyMutation {{
-           createNews (input: {{title: "{title}", description: "{description}", link: "{link}", pubDate: "{pubDate}"}}) {{
-              description 
-              link 
-              pubDate 
-              title
-            }}
-        }}"""
-    print(query)
 
     # Now we can simply post the request for the mutation to the API...
-    response = session.request(
-    url=APPSYNC_API_ENDPOINT_URL,
-    headers={'x-api-key': ''},
-    method='POST',
-    json={'query': query})
-    print(response.json())
+    #response = session.request(
+    #url=APPSYNC_API_ENDPOINT_URL,
+    #headers={'x-api-key': ''},
+    #method='POST',
+    #json={'query': query})
+    #print(response.json())
